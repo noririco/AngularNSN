dfbb59a633596b43fe02b2b2da3454bcb3207b48
NSN <nisanlevy666@gmail.com>
2017-11-21 19:21:51 +0200

    assets worng path in AppComponent, now fixed to right path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   myLazyImage: any;
   constructor() {
-    this.myLazyImage = '../assets/images/BG_fixed.png';
+    this.myLazyImage = 'assets/images/BG_fixed.png';
    }
   title = 'app';
   ngOnInit() {
